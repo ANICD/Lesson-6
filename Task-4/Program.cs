2dc2aec0efb71e89ea97f586b8600cb1d329c705
Dovgal Andrey <a.nic.dovgal@gmail.com>
2023-12-29 00:23:13 +0300

    Commit for Task-4

---
+++ Task-4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами.
+//Сформировать строку, в которой слова расположены в обратном порядке. В полученной строке слова должны быть также разделены пробелами.
+Console.WriteLine("Напишите фразу: ");
+string inputString = Console.ReadLine();
+string[] wordsArr = inputString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+int len = wordsArr.Length;
+//----------------------------------------
+for (int i = 0; i < len / 2; i++)
+{
+    string tempString = wordsArr[i];
+    wordsArr[i] = wordsArr[len - 1 - i];
+    wordsArr[len - 1 - i] = tempString;
+}
+for (int i = 0; i < len; i++)
+{
+    Console.Write(wordsArr[i] + " ");
+}
+//----------------------------------------
+Console.WriteLine();
